fedcad370e37aff0478a77022a30cb56e54c6482
Cristian Marsico <cristianmarsico84@gmail.com>
2023-04-10 17:30:21 -0300

    configuracion de puerto

---
+++ index.js
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import express from "express";
+const app = express()
+
+//VAMOS A TRABAJAR CON JSON
+app.use(express.json())
+    //PARA PROCESAR DATOS ENVIADOS DESDE FORMS
+    .use(express.urlencoded({
+        extended: true
+    }))
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`👍👍👍 Escuchando en el puerto ${PORT}`)
+})
